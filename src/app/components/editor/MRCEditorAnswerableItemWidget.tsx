10f673690b6e584b2952d63776a3ca0ffc4f6856
Enrico <enrico.bottani@hotmail.com>
2022-03-03 13:49:36 +0100

    Layout fixes

--- src/app/components/editor/MRCEditorAnswerableItemWidget.tsx
+++ src/app/components/editor/MRCEditorAnswerableItemWidget.tsx
@@ -1,19 +1,24 @@
 import MRCAnswerableItem from "../../model/MRCAnswerableItem";
 
 export default function MRCEditorAnswerableItemWidget(props: { answerableItem: MRCAnswerableItem }) {
-    function onChange(e: React.ChangeEvent<HTMLInputElement>, id: number) {
-        console.log("change: "+e.target.value);
+    function onChange(e: React.ChangeEvent<HTMLInputElement>) {
+        console.log("change: " + e.target.value);
         console.log(props.answerableItem);
     }
 
-    return (<div className="input-group rounded-0 mb-1" key={props.answerableItem.id}>
-        <div className="input-group-text rounded-0">
-            <input className="form-check-input mt-0" type="radio" name="radio-x"
-                   aria-label="Radio button for following text input"/>
-        </div>
-        <input type="text" defaultValue={props.answerableItem.choice}
-               onChange={e => onChange(e, props.answerableItem.id)}
-               className="form-control rounded-0"
-               aria-label="Text input with radio button"/>
-    </div>);
+    return (
+        <div>
+            <div className="input-group rounded-0 mb-1" key={props.answerableItem.id}>
+                <div className="input-group-text rounded-0">
+                    <input className="form-check-input mt-0" type="radio" name="radio-x"
+                           aria-label="Radio button for following text input"/>
+                </div>
+                <input type="text" defaultValue={props.answerableItem.choice}
+                       onChange={e => onChange(e)}
+                       className="form-control rounded-0"
+                       aria-label="Text input with radio button"/>
+                <button type="button" className="btn btn-outline-secondary rounded-0"><i className="bi bi-cloud-upload"/></button>
+                <button type="button" className="btn btn-outline-danger rounded-0"><i className="bi bi-trash3"/></button>
+            </div>
+        </div>);
 }
